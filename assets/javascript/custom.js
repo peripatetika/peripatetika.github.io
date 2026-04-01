418acde10a9659f38ad78e37fdab704a4972a017
peripatetika <peripatetika.team@gmail.com>
2026-04-01 18:31:37 +0200

    Deployed 0328b2f with MkDocs version: 1.6.1

---
+++ assets/javascript/custom.js
@@ -0,0 +1,15 @@
+// za verziju sa strelicom
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("main a").forEach(el => {
+    // If the visible text equals the Croatian translation
+    if (el.textContent.trim() === "Natrag na indeks") {
+      // Replace the text
+      el.textContent = "Natrag na početnu";
+      
+      // Add the left arrow before the text
+      const arrow = document.createElement("span");
+      arrow.textContent = "‹"; // Unicode left arrow
+      el.prepend(arrow);
+    }
+  });
+});
